7c921675ef46c8269df4501610c7c59019c5ad09
João Pedro <js675914@gmail.com>
2026-05-27 13:11:20 -0300

    feat(frontend): iniciando projeto React com Vite, Tailwind CSS e Axios

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-md">
+        FilaZero + Tailwind v4 Funcionando!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
